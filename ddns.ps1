5029ffc2b12448bde141bd4800c047cddd080073
IsaacFL <IsaacFL@users.noreply.github.com>
2018-04-03 14:07:25 -0700

    Shortened and it works

--- ddns.ps1
+++ ddns.ps1
@@ -1,55 +1,33 @@
-﻿$HostName = "isv6.psp.iznmort.com"
-$UserName = "s8V2ThySz2z9Wz2L"
-$Password = "7X5SPwl0xQlMJqaA"
+﻿# Vars
+$hostname  = "isv6.psp.iznmort.com"
+$user  = "s8V2ThySz2z9Wz2L"
+$pass  = "7X5SPwl0xQlMJqaA"
 
+# Get Current IP Addresses
 $ip4 = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
 $ip6 = Get-NetIPAddress -AddressFamily ipv6 -AddressState Preferred -PrefixOrigin RouterAdvertisement -SuffixOrigin link
 
     
- 
-$URL = "https://{0}:{1}@domains.google.com/nic/update?hostname={2}&myip={3}" -F $UserName, $Password, $HostName, $ip6
+# Basic Auth Formatting
+$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($user, $secpasswd)
 
-write-host $URL
-Write-host "Sending update request to Google DNS..."
 
-$Response = Invoke-WebRequest $URL
-    $Result = $Response.Content
-    $StatusCode = $Response.StatusCode
+# Get IP4 First
+$ipPub = $ip4
 
-write-host $Result, $StatusCode
-pause
-    
+# Output
+"Updating Host " + $hostname + " to: " + $ipPub
+$URL = "https://domains.google.com/nic/update?hostname=" + $hostname + "&myip=" + $ipPub
+$result = Invoke-RestMethod $URL -Credential $credential
+"Server Response: " + $result
 
-    switch ($Result) {
-      "good*" { 
-        $splitResult = $Result.split(" ")
-        $newIp = $splitResult[1]
-        Write-Verbose "IP successfully updated for $HostName to $newIp."
-      }
-      "nochg*" {
-        $splitResult = $Result.split(" ")
-        $newIp = $splitResult[1]
-        Write-Verbose "No change to IP for $HostName (already set to $newIp)."
-      }
-      "badauth" {
-        throw "The username/password you provided was not valid for the specified host."
-      }
-      "nohost" {
-        throw "The hostname you provided does not exist, or dynamic DNS is not enabled."
-      }
-      "notfqdn" {
-        throw "The supplied hostname is not a valid fully-qualified domain name."
-      }
-      "badagent" {
-        throw "You are making bad agent requests, or are making a request with IPV6 address (not supported)."
-      }
-      "abuse" {
-        throw "Dynamic DNS access for the hostname has been blocked due to failure to interperet previous responses correctly."
-      }
-      "911" {
-        throw "An error happened on Google's end; wait 5 minutes and try again."
-      }
-    }
- 
- 
 
+# Get IP6 First
+$ipPub = $ip6.ipaddress
+
+# Output
+"Updating Host " + $hostname + " to: " + $ipPub
+$URL = "https://domains.google.com/nic/update?hostname=" + $hostname + "&myip=" + $ipPub
+$result = Invoke-RestMethod $URL -Credential $credential
+"Server Response: " + $result
