e3acb38dbe92ffdc6c0b60d863837dbaaf4b0b48
IsaacFL <IsaacFL@users.noreply.github.com>
2018-04-06 16:36:17 -0700

    working with using local resolver

--- ddns.ps1
+++ ddns.ps1
@@ -2,15 +2,17 @@
 $hostname  = "isaacfl"
 $token  = "0ee3b92c-97f6-4555-9e1e-b5483e18ebc6"
 
+$fqdn = $hostname + ".psp.iznmort.com"
+
 # Get Current IP Addresses
 $ip4 = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
-$ip6 = Get-NetIPAddress -AddressFamily ipv6 -AddressState Preferred -PrefixOrigin RouterAdvertisement -SuffixOrigin link
-
+$ip6 = (Resolve-DnsName $fqdn -Server "192.168.1.1" -Type AAAA).IPAddress
 
 "Updating Host " + $hostname + " to: "
 write-host "IP4 " $ip4
-write-host "IP6 " $ip6.ipaddress
-$URL = "https://www.duckdns.org/update?domains=" + $hostname + "&token=" + $token + "&ip=" + $ip4 + "&ipv6=" + $ip6.ipaddress
+write-host "IP6 " $ip6
+
+$URL = "https://www.duckdns.org/update?domains=" + $hostname + "&token=" + $token + "&ip=" + $ip4 + "&ipv6=" + $ip6
 $result = Invoke-RestMethod $URL -Credential $credential
 "Server Response: " + $result
 pause
