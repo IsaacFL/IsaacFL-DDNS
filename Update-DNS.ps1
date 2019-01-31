687e38b49e11c5d7f775f1d95f0cf861145783ab
IsaacFL <IsaacFL@users.noreply.github.com>
2019-01-30 21:15:05 -0800

    Update Update-DNS.ps1

--- Update-DNS.ps1
+++ Update-DNS.ps1
@@ -28,53 +28,41 @@ $Zone_Identifier = $Zone_Identifier.Trim()
 $Config.records | ForEach-Object {
     $Record_Name = $_
 
-     # Resolve Current IP Address Locally
+    # Resolve Current IP Address Locally
     $IP = (Resolve-DnsName $Record_Name -Type AAAA).IPAddress
+
     # Resolve Current IP Address Externally
     $onlineip = (Resolve-DnsName $Record_Name -Type AAAA -Server $extDNS ).IPAddress
 
-    Write-Log ($Record_Name + " Local IP " + $IP)
-    Write-Log ($Record_Name + " External IP " + $onlineip)
 
-
-    if ($IP -ne $onlineip) {
-        $Message = $Record_Name + " Updating Host to: " + $IP
-        Write-Log $Message
-        $Uri = "https://api.cloudflare.com/client/v4/zones/" + $Zone_Identifier + "/dns_records?name=" + $Record_Name
-        Write-Log $URI
-        }
-    else {
-        $Message = $Record_NAme + " No Change: " + $onlineip
-        Write-Log $Message
-        }
-}
-
-<#
-
-$Config.records | ForEach-Object {
-    $Record_Name = $_
     $Uri = "https://api.cloudflare.com/client/v4/zones/" + $Zone_Identifier + "/dns_records?name=" + $Record_Name
     $Response = Invoke-RestMethod -Uri $Uri -Headers $Header
     $Record_Identifier = $Response.result[0].id
 
-    $Uri = "https://api.cloudflare.com/client/v4/zones/" + $Zone_Identifier + "/dns_records/" + $Record_Identifier
-    $Body = @{"id" = $Zone_Identifier; "type" = "A"; "name" = $Record_Name; "content" = $IP; "proxied" = $FALSE} | ConvertTo-Json
-    try {
-        $Response = Invoke-RestMethod -Method Put -Uri $Uri -Headers $Header -Body $Body
-    } catch {
-        $result = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($result)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
-        $Body = $responseBody | ConvertTo-Json
 
-        $Message = "ERROR on " + $Record_Name + " Response: " + $Body
+    if ($IP -ne $onlineip) {
+        $Uri = "https://api.cloudflare.com/client/v4/zones/" + $Zone_Identifier + "/dns_records/" + $Record_Identifier
+        $Body = @{"id" = $Zone_Identifier; "type" = "AAAA"; "name" = $Record_Name; "content" = $IP;} | ConvertTo-Json
+
+        $Message = "Updating: " + $Record_NAme + " " + $IP
         Write-Log $Message
-    }
+            
+        try {
+            $Response = Invoke-RestMethod -Method Put -Uri $Uri -Headers $Header -Body $Body
+            } catch {
+                $result = $_.Exception.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($result)
+                $reader.BaseStream.Position = 0
+                $reader.DiscardBufferedData()
+                $responseBody = $reader.ReadToEnd();
+                $Body = $responseBody | ConvertTo-Json
+
+                $Message = "ERROR on " + $Record_Name + " Response: " + $Body
+                Write-Log $Message
+                }
+        }
+    else {
+        $Message = "No Change: " + $Record_NAme + " " + $onlineip
+        Write-Log $Message
+        }
 }
-
-$Message = "IP changed to " + $IP
-Write-Log $Message
-
-#>
